2d8d83f3641704b361797d7151fd0af5581f364e
Grace Zheng <gracexzheng2011@gmail.com>
2026-04-13 20:09:28 -0400

    cc

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ function sendData(accuracy, totalTime) {
   const payload = {
     user_id: localStorage.getItem("user_id"),
     name: localStorage.getItem("name") || "Anonymous",
-    age: localStorage.getItem("hours") || "Unknown",
+    hours: localStorage.getItem("hours") || "Unknown",
     score: score,
     accuracy: accuracy,
     time: totalTime
@@ -195,3 +195,5 @@ fetch("/count")
     document.getElementById("counter").innerText =
       "amt of ppl who took this test: " + data.count;
   });
+
+localStorage.clear();
